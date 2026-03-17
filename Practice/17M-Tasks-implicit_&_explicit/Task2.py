bfe16b067b48eaa81448c26124bd6e93601bbbe2
bhavik <sirohiyabhavik@gmail.com>
2026-03-17 23:33:41 +0530

    Implicit and Explicit wait

---
+++ Practice/17M-Tasks-implicit_&_explicit/Task2.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver import Chrome, ChromeOptions
+o = ChromeOptions()
+o.add_experimental_option("detach", True)
+driver = Chrome(options=o)
+
+driver.maximize_window()
+
+driver.get("https://demoqa.com/webtables")
+
+wait = WebDriverWait(driver, 10)
+print(wait.until(EC.visibility_of_element_located((By.XPATH, '//td[.="Kierra"]/../td[5]'))).text)
+
+# OR
+
+# print(wait.until(EC.visibility_of_element_located((By.XPATH, '//td[text()="Kierra"]//following-sibling::td[4]'))).text)
+
+driver.quit()
