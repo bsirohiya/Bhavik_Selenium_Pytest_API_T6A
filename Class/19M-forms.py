17750253873f3da0b60b2ca0a339c91809bc9111
bhavik <sirohiyabhavik@gmail.com>
2026-03-24 21:37:17 +0530

    Frames and Alerts(Popups)

--- Class/19M-forms.py
+++ Class/19M-forms.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 
@@ -7,7 +9,7 @@ o.add_experimental_option("detach", True)
 driver = Chrome(options=o)
 driver.maximize_window()
 
-driver.get("https://demoqa.com/automation-practice-form")
+'''driver.get("https://demoqa.com/automation-practice-form")
 
 driver.implicitly_wait(10)
 
@@ -22,4 +24,29 @@ driver.find_element(By.XPATH, '//input[@id="subjectsInput"]').send_keys("7418529
 driver.find_element(By.XPATH, '//input[@id="hobbies-checkbox-1"]').click()
 driver.find_element(By.XPATH, '//input[@id="hobbies-checkbox-3"]').click()
 driver.find_element(By.XPATH, '//input[@id="uploadPicture"]').send_keys(r"D:\Extra work\CORE Subjects.docx")
-driver.find_element(By.XPATH, '//textarea[@id="currentAddress"]').send_keys("helloooooooooooo")
+driver.find_element(By.XPATH, '//textarea[@id="currentAddress"]').send_keys("helloooooooooooo")'''
+
+
+'''
+# For selecting date
+driver.get("https://www.irctc.co.in/nget/train-search")
+selectDate = driver.find_element(By.XPATH, '//input[@class="ng-tns-c69-9 ui-inputtext ui-widget ui-state-default ui-corner-all ng-star-inserted"]')
+selectDate.click()
+forwardBtn = driver.find_element(By.XPATH, '//span[@class="ui-datepicker-next-icon pi pi-chevron-right ng-tns-c69-9"]')
+forwardBtn.click()
+
+date = driver.find_element(By.XPATH, '(//a[@class="ui-state-default ng-tns-c69-9 ng-star-inserted"])[1]')
+date.click()
+'''
+
+
+driver.get("https://demoqa.com/automation-practice-form")
+driver.find_element(By.XPATH, '//input[@id="dateOfBirthInput"]').click()
+
+driver.find_element(By.XPATH, '//select[@class="react-datepicker__month-select"]').click()
+driver.find_element(By.XPATH, '//option[.="May"]').click()
+
+driver.find_element(By.XPATH, '//select[@class="react-datepicker__year-select"]').click()
+driver.find_element(By.XPATH, '//option[.="2004"]').click()
+
+driver.find_element(By.XPATH, '//div[.="15"]').click()
---
+++ Practice/24M-Tasks/Problem_Statements_8.md
@@ -0,0 +1,31 @@
+## Task 1
+
+Write a selenium script to launch Twitter website and perform:
+1. Using iFrame switch to child frame 
+2. Click on Sign up with Google
+
+
+---
+
+## Task 2
+
+Write a selenium script to launch ZOMATO website and perform:
+
+1. Click on 'Log in'
+2. Using iFrame go inside the child frame
+3. Click on Sign to get OTP
+
+---
+
+
+## TASK 3: 
+1. Open 3 websites in separate tabs.
+2. Print each tabs title and URL. 
+3. Close all Tabs except the first tab
+
+
+## TASK 4:
+
+1. Open https://the-internet.herokuapp.com/ then JS alert.
+2. Inside that try all 3 alert options.  
+3. Fetch the text from those alerts.
